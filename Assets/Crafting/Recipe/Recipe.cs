e362615b21dd4ac846ec0d7c197402167dc6db9d
MMU <MMU@DESKTOP-73Q57U2>
2023-05-31 17:35:48 +0800

    recipe

--- Assets/Crafting/Recipe/Recipe.cs
+++ Assets/Crafting/Recipe/Recipe.cs
@@ -7,6 +7,9 @@ using UnityEngine.UI;
 public class Recipe : MonoBehaviour
 {
 
+    public TextMeshProUGUI RecipeTitleText;
+    public Button CraftButton;
+
     public GameObject Ingredient1;
     public GameObject Ingredient2;
     public GameObject Ingredient3;
